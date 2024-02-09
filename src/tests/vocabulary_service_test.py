f101752da89502b0a8dac0eed00393682826e17e
Olivia Brotherus <olivia@brotherus.fi>
2024-02-09 15:28:17 +0200

    Revert "testien muokkaamista"
    This reverts commit 6ce72187bad0ebffe673932dd3b0f32883588168.

--- src/tests/vocabulary_service_test.py
+++ src/tests/vocabulary_service_test.py
@@ -11,39 +11,33 @@ class TestVocabularyService(unittest.TestCase):
         self.trie_mock = Mock(wraps=Trie())
         self.damerau_levenshtein_mock = Mock(wraps=DamerauLevenshtein())
         self.vocabulary_service = VocabularyService("sanasto_test.csv", self.trie_mock, self.damerau_levenshtein_mock)
-    
-    def test_find_similar_words_with_none(self):
-        similar_words = self.vocabulary_service.find_similar_words(None)
-        self.damerau_levenshtein_mock.distance.assert_not_called()
-        self.assertEqual(similar_words, [])
 
     def test_find_similar_words(self):
         self.vocabulary_service.find_similar_words("kisda")
         self.damerau_levenshtein_mock.distance.assert_called()
 
     def test_find_similar_words_with_empty_string(self):
+        similar_words = self.vocabulary_service.find_similar_words(None)
+        self.assertEqual(similar_words, [])
+
+    def test_find_similar_words_with_none(self):
         similar_words = self.vocabulary_service.find_similar_words("")
-        self.damerau_levenshtein_mock.distance.assert_not_called()
         self.assertEqual(similar_words, [])
 
     def test_find_word_in_vocabulary(self):
-        self.vocabulary_service.find_word_in_vocabulary("kissa")
-        self.trie_mock.search.assert_called()
-
-    def test_add_word_to_vocabulary_with_none(self):
-        times_called = self.trie_mock.add.call_count
-        self.assertFalse(
-            self.vocabulary_service.add_word_to_vocabulary(None))
-        self.assertEqual(self.trie_mock.add.call_count, times_called)
-
+            self.vocabulary_service.find_word_in_vocabulary("kissa")
+            self.trie_mock.search.assert_called()
+        
     def test_add_word_to_vocabulary(self):
         random_word = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=5))
         self.assertTrue(
             self.vocabulary_service.add_word_to_vocabulary(random_word))
-        self.trie_mock.add.assert_called_with(random_word)
+    
+    def test_add_word_to_vocabulary_with_none(self):
+        self.assertFalse(
+            self.vocabulary_service.add_word_to_vocabulary(None))
         
     def test_add_existing_word_to_vocabulary(self):
         self.assertFalse(
             self.vocabulary_service.add_word_to_vocabulary("kissa"))
-        
         
