1d0a557029ed2f5acfe10f2864958c26a9cf7105
Olivia Brotherus <olivia@brotherus.fi>
2024-02-09 15:26:31 +0200

    invarianttitestausta

--- src/tests/vocabulary_service_test.py
+++ src/tests/vocabulary_service_test.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock, ANY
+from unittest.mock import Mock
 import random
 from services.vocabulary_service import VocabularyService
 from services.trie import Trie
